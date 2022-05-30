fe532a73253098133f1c145ff27e2f9547cfe5c0
VorontsovS <3329050@gmail.com>
2022-05-30 05:58:57 +0300

    2 commit

--- script/main.js
+++ script/main.js
@@ -10,6 +10,8 @@ const txtPut =  '<div>You put the cheese. Please, close the door.<div/>';
 
 const txtWrong = '<div>Wrong !!! Plesase, open the door.<div/>';
 
+let valueDoor = elDoor.value;
+
 const runCheeseOpen = () => {
     const valueCheese = elCheese.value;
 
@@ -39,20 +41,19 @@ const runCheeseClose = () => {
 };
 
 const runDoor = () => {
-    const valueDoor = elDoor.value;
+    let valueDoor = elDoor.value;
 
     if (valueDoor === 'open') {
         elAlgoritm.insertAdjacentHTML('beforeend', txtOpen);
-        elCheese.addEventListener('change', runCheeseOpen);
+        elCheese.addEventListener('change', runCheeseOpen, { once: true });
         return;
     } 
     
     if (valueDoor === 'close') {
         elAlgoritm.insertAdjacentHTML('beforeend', txtClose);
-        elCheese.addEventListener('change', runCheeseClose);
+        elCheese.addEventListener('change', runCheeseClose, { once: true });
         return;
     }
-    return;
 };
 
 elDoor.addEventListener('change', runDoor);
